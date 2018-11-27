903f3137f19d1f551419f02fe56458d13996a48c
Red <243309672@qq.com>
2018-11-27 10:22:54 +0800

    spring cloud学习

---
+++ client/src/main/java/com/eureka/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package com.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+}
